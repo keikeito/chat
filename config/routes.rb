aec4c2a6c5f3789d2229351cb69b256c6bac7018
keikeito <n.keikeito0122@gmail.com>
2017-11-29 08:04:32 +0900

    レビュー修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'messages#index'
-resources :users, only: [:index, :edit, :update]
+resources :users, only: [:edit, :update]
 
 end
