3b1dd9d5be7306d7435870b1dffb391b2e58f05d
keikeito <n.keikeito0122@gmail.com>
2017-12-09 16:31:49 +0900

    グループ作成機能の実装

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -1,9 +1,21 @@
 class ChatGroupsController < ApplicationController
-  def index
-
-  end
-
   def new
     @group = Group.new
   end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to controller: 'messages', action: 'index'
+      flash[:notice] = "グループを作成しました"
+    else
+      render "new"
+    end
+
+  end
+
+  def group_params
+    params.require(:group).permit(:group_name, user_ids: [])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171203224112) do
+ActiveRecord::Schema.define(version: 20171209071851) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "group_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
