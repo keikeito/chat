83f09a64f88337bffc721425d8551bcd5c01949b
keikeito <n.keikeito0122@gmail.com>
2017-12-09 17:18:23 +0900

    レビュー修正

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -6,8 +6,7 @@ class ChatGroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to controller: 'messages', action: 'index'
-      flash[:notice] = "グループを作成しました"
+      redirect_to root_path, notice: "グループを作成しました"
     else
       render "new"
     end
