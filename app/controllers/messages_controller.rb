e215800f4f6a013a2f8604e9c27b2c5a6453bc1a
keikeito <n.keikeito0122@gmail.com>
2018-01-06 11:15:58 +0900

    レビュー修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def index
-    @group = ChatGroup.find(params[:chat_group_id]);
+    @group = ChatGroup.find(params[:chat_group_id])
     @messages = Message.where(chat_group_id: params[:chat_group_id])
     @message = Message.new
   end
@@ -21,6 +21,7 @@ class MessagesController < ApplicationController
 
   end
 
+  private
   def message_params
     params.require(:message).permit(:body, :image).merge(chat_group_id: params[:chat_group_id]).merge(user_id: current_user.id)
   end
